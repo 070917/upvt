66a4296adda7d1a79288f264f907826c09253091
Daniel Acuña <daniel.leonardoacua@gmail.com>
2020-11-22 14:11:11 -0600

    desvincular
    se alñidieron las intrucciones para desvincular fb

---
+++ pag/deleteuser.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Desvincular mi cuneta con facebook</title>
+  </head>
+  <body>
+    <h1>Desvincular mi cuneta con facebook</h1>
+    <p>A continuacion explicaremos como Desvincular tu cuneta.</p>
+    
+  </body>
+</html>
