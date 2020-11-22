73d6599b333ab69fd85640cbd674f89b2b692927
Daniel Acuña <daniel.leonardoacua@gmail.com>
2020-11-22 15:52:33 -0600

    No existe directorio ERR
    Se cambio el directorio de la URL

--- pag/login.php
+++ pag/login.php
@@ -43,7 +43,7 @@ $fb = new Facebook\Facebook([
 $helper = $fb->getRedirectLoginHelper();
 
 $permissions = ['email']; // Optional permissions
-$redirectURL = "https://".$_SERVER['SERVER_NAME']."/fb-callback.php";
+$redirectURL = "https://".$_SERVER['SERVER_NAME']."/pag/fb-callback.php";
 $loginUrl = $helper->getLoginUrl($redirectURL, $permissions);
 echo '<button> <img src="fb.png"> <a href="' . $loginUrl . '">Log in con Facebook!</a></button>';
 
